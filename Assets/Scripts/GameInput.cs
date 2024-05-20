be4a689ee77bc9dacfc5a175b36ff66f9a35a3c1
diptodev <127576890+diptodev@users.noreply.github.com>
2024-05-20 14:01:06 +0600

    Game Input Separated

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+   public Vector2 GetNormalizedInput()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            inputVector.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            inputVector.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            inputVector.x += 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            inputVector.y += 1;
+        }
+
+        return inputVector.normalized;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,26 +7,11 @@ public class Player : MonoBehaviour
    private float moveSpeed=5f;
     private float rotateSpeed = 15f;
     private bool isWalking = false;
+    [SerializeField] private GameInput gameInput;
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.DownArrow)){
-            inputVector.y -= 1;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)){
-            inputVector.x -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow)){
-
-            inputVector.x += 1;
-        }
-        if (Input.GetKey(KeyCode.UpArrow)){
-            inputVector.y += 1;
-        }
-        
-         inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetNormalizedInput();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         transform.position += moveDir*Time.deltaTime*moveSpeed;
