3ba01728def1b728f3b1ca3be3e0694fde4e259d
diptodev <127576890+diptodev@users.noreply.github.com>
2024-05-20 13:19:33 +0600

    rotation added to the project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
    private float moveSpeed=5f;
+    private float rotateSpeed = 15f;
     [SerializeField] Animator animator;
     // Update is called once per frame
     void Update()
@@ -33,7 +34,9 @@ public class Player : MonoBehaviour
             animator.SetBool("isWalk", false);
         }
          inputVector = inputVector.normalized;
-        Vector3 newPos = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += newPos*Time.deltaTime*moveSpeed;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += moveDir*Time.deltaTime*moveSpeed;
+        transform.forward=Vector3.Lerp(transform.forward,moveDir,Time.deltaTime*rotateSpeed);
     }
 }
