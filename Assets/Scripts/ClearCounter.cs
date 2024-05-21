b864727f7ae346d245265f8ea7183b2666511816
diptodev <127576890+diptodev@users.noreply.github.com>
2024-05-21 23:05:57 +0600

    Player can Grab object from Clearcounter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,11 +8,30 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
+        //ClearCounter has nothing
         if (!HasKitchenObject())
         {
+            //Player has something
             if (player.HasKitchenObject())
             {
                 player.GetKitchenObject().SetIKitchenObjectParent(this);
+            }
+            else
+            {
+                //Player has Nothing
+            }
+        }
+        else
+        {
+            //Player Has Nothing
+            if(HasKitchenObject())
+            {
+                //ClearCounter has something and give it to the player
+                GetKitchenObject().SetIKitchenObjectParent(player);
+            }
+            else
+            {
+                //ClearCounter has nothing
             }
         }
     }
