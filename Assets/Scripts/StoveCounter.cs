6087a76c2d530ea5b3790b5d184d20c835b6f6d0
diptodev <127576890+diptodev@users.noreply.github.com>
2024-05-23 11:00:36 +0600

    Stove Counter Added

---
+++ Assets/Scripts/StoveCounter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounter : BaseCounter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public override void Interact(Player player) { }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
